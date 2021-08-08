737627509cef3347982648a001b2c04e87788bb1
rYuu-H <holungsang@yahoo.com>
2021-08-08 20:33:57 +0800

    JFrame!

--- Main.java
+++ Main.java
@@ -1,6 +1,17 @@
+import javax.swing.JFrame;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("hello git!");
+        //gui for sorter
+        JFrame frame = new JFrame();
+        frame.setTitle("Sorter");
+        frame.setSize(400,800);
+        frame.setVisible(true);
+
+
+        
+
     }
 }
